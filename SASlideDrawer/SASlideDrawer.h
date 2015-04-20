297caf50770aa1ac7ac7004da7cdceea75444b24
Stefan Arambasich <ArtisOracle@gmail.com>
2015-04-20 14:44:55 -0400

    Initial Commit

---
+++ SASlideDrawer/SASlideDrawer.h
@@ -0,0 +1,19 @@
+//
+//  SASlideDrawer.h
+//  SASlideDrawer
+//
+//  Created by Stefan Arambasich on 4/20/2015.
+//  Copyright (c) 2015 Stefan Arambasich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SASlideDrawer.
+FOUNDATION_EXPORT double SASlideDrawerVersionNumber;
+
+//! Project version string for SASlideDrawer.
+FOUNDATION_EXPORT const unsigned char SASlideDrawerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SASlideDrawer/PublicHeader.h>
+
+
---
+++ SASlideDrawerTests/SASlideDrawerTests.swift
@@ -0,0 +1,36 @@
+//
+//  SASlideDrawerTests.swift
+//  SASlideDrawerTests
+//
+//  Created by Stefan Arambasich on 4/20/2015.
+//  Copyright (c) 2015 Stefan Arambasich. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SASlideDrawerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
